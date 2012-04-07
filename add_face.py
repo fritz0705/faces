0244973172a6426d9500d095961860965ad9a1c6
Fritz Grimpen <fritz@grimpen.net>
2012-04-07 02:45:11 +0000

    Changed full size to 460x460

--- add_face.py
+++ add_face.py
@@ -6,7 +6,7 @@ import argparse
 import subprocess
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('file')
+argparser.add_argument('files', nargs='*')
 argparser.add_argument('--tag', '-t', action='append')
 
 db = pymongo.Connection().clintonfaces
@@ -14,14 +14,15 @@ db = pymongo.Connection().clintonfaces
 if __name__ == '__main__':
 	args = argparser.parse_args()
 
-	number = 1
-	while True:
-		if db.faces.find({"number": number}).count() == 0:
-			break
-		number = number + 1
-	obj = { "number": number, "tags": args.tag or [] }
-	db.faces.save(obj)
-	
-	# build image
-	subprocess.call(['convert', args.file, '-resize', '600x600^', '-gravity', 'center', '-extent', '600x600', 'faces/{0}.jpg'.format(number)])
-	subprocess.call(['convert', 'faces/{0}.jpg'.format(number), '-size', '150x150', 'thumbs/{0}.jpg'.format(number)])
+	for file in args.files:
+		number = 1
+		while True:
+			if db.faces.find({"number": number}).count() == 0:
+				break
+			number = number + 1
+		obj = { "number": number, "tags": args.tag or [] }
+		db.faces.save(obj)
+		
+		# build image
+		subprocess.call(['convert', file, '-resize', '460x460^', '-gravity', 'center', '-extent', '460x460', 'faces/{0}.jpg'.format(number)])
+		subprocess.call(['convert', 'faces/{0}.jpg'.format(number), '-size', '150x150', 'thumbs/{0}.jpg'.format(number)])
