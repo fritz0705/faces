712631816036611f555b761a718d4a1f410f603f
Fritz Grimpen <fritz@grimpen.net>
2012-04-07 12:14:23 +0000

    Bugfix

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 $(function() {
 	var face_url_e = $("input#face_url");
 	var url_prefix = window.location.protocol + "//" + window.location.host;
-	$("a").click(function() {
+	$(".face a").click(function() {
 		var url = url_prefix + $(this).attr('href');
 		face_url_e.val(url);
 		face_url_e.select();
