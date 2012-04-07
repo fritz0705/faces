9cdf6064c23b647c81642193c8ce5fba5b58ab59
Fritz Grimpen <fritz@grimpen.net>
2012-04-07 14:28:19 +0000

    Added use strict

--- static/main.js
+++ static/main.js
@@ -1,14 +1,15 @@
-$(function() {
-	var face_url_e = $("input#face_url");
-	var url_prefix = window.location.protocol + "//" + window.location.host;
-	$(".face a").click(function() {
+$(function () {
+	"use strict";
+	var face_url_e = $("input#face_url"),
+		url_prefix = window.location.protocol + "//" + window.location.host;
+	$(".face a").click(function () {
 		var url = url_prefix + $(this).attr('href');
 		face_url_e.val(url);
 		face_url_e.select();
 		return false;
 	});
 
-	$(".face a").dblclick(function() {
+	$(".face a").dblclick(function () {
 		window.location.replace(url_prefix + $(this).attr('href'));
 	});
 });
