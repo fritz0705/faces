3c97524e5ad59c1b83b86c3fe40da756efe109f1
Fritz Grimpen <fritz@grimpen.net>
2012-04-07 12:21:59 +0000

    Fixed minified js

---
+++ static/main.comp.js
@@ -0,0 +1,1 @@
+$(function(){var a=$("input#face_url"),b=window.location.protocol+"//"+window.location.host;$(".face a").click(function(){var c=b+$(this).attr("href");a.val(c);a.select();return!1})});
