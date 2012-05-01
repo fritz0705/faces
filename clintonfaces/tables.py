c357c173deea9be20362335fe389bedac1593e76
Fritz Grimpen <fritz@grimpen.net>
2012-05-01 15:48:27 +0000

    Removed old sqlalchemy foo

--- clintonfaces/tables.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-from sqlalchemy import Table, Column, Integer, String, MetaData
-
-metadata = MetaData()
-
-faces = Table('faces', metadata,
-		Column('id', Integer, primary_key=True)
